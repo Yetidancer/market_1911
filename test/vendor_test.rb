3dd5e89b5b5fa3fe5f8d7c6ab1c1a0b425353534
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-14 09:14:27 -0700

    Add test it exists for vendor class

---
+++ test/vendor_test.rb
@@ -0,0 +1,21 @@
+require './vendor'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    
+  end
+
+end
